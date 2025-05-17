ef043ed393da87e55a678345753dbd62ab83e585
fengzebing <1390653296@qq.com>
2025-05-17 21:52:13 +0800

    feat: init project

---
+++ app/AppProvider.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
+
+// export const devtools = process.env.NODE_ENV === "development" && typeof window !== "undefined" ? React.createElement(ReactQueryDevtools, {}) : null;
+// export const devtools2 = process.env.NODE_ENV === "development" && typeof window !== "undefined" ? React.createElement(ReactQueryDevtools, {}) : null;
+const queryClient = new QueryClient();
+
+export function AppProvider({children}: { children: React.ReactNode }) {
+    return (
+        <QueryClientProvider client={queryClient}>
+            {children}
+            <ReactQueryDevtools initialIsOpen={false}/>
+        </QueryClientProvider>
+    );
+}
